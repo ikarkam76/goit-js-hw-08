9ff7c11bbde938c1d63015c52b9a04efcdafed1b
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-17 20:36:40 +0300

    corrected 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,29 +3,51 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player', {});
 
-// не знаю який варіант вірний
 
-// --------------- перший варіант ------------
-
-// const onPlay = (evt) => {
-//     localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
-// };
-
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-// const parsedTime = JSON.parse(savedTime);
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-// player.setCurrentTime(parsedTime.seconds);
-
-// -----другий варіант, він також працює, і код чистіше ------------
-
-const onPlay = evt => {
-    localStorage.setItem('videoplayer-current-time', evt.seconds);
+const onPlay = (evt) => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
 };
-
-const savedTime = localStorage.getItem('videoplayer-current-time');
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(savedTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+
+console.log(parsedTime);
+
+player.setCurrentTime(parsedTime.seconds).then(function (seconds) {
+    }).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+// -----другий варіант, він також працює, і код чистіше ------------
+
+// const onPlay = evt => {
+//     localStorage.setItem('videoplayer-current-time', evt.seconds);
+// };
+
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// player
+//   .setCurrentTime(savedTime)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
