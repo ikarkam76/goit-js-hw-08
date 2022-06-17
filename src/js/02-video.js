466a478676c620dd6c31dd44495fc89916951aa6
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-17 16:33:03 +0300

    finished 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,32 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {});
 
+// не знаю який варіант вірний
 
+// --------------- перший варіант ------------
 
+// const onPlay = (evt) => {
+//     localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
+// };
 
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+// const parsedTime = JSON.parse(savedTime);
+// console.log(parsedTime);
 
+// player.on('timeupdate', onPlay);
+
+// player.setCurrentTime(parsedTime.seconds);
+
+// -----другий варіант, він також працює, і код чистіше ------------
+
+const onPlay = evt => {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+};
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+console.log(savedTime);
+
+player.on('timeupdate', onPlay);
+
+player.setCurrentTime(savedTime);
