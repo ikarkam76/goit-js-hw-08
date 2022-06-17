b9554bbdc46b9553c720100db442ec20bafbc7da
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-17 20:58:09 +0300

    correcter 02 comments

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,30 +3,6 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player', {});
 
-
-// const onPlay = (evt) => {
-//     localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
-// };
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-// const parsedTime = JSON.parse(savedTime);
-
-// console.log(parsedTime);
-
-// player.setCurrentTime(parsedTime.seconds).then(function (seconds) {
-//     }).catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         break;
-//       default:
-//         break;
-//     }
-//   });
-// -----другий варіант, він також працює, і код чистіше ------------
-
 const onPlay = evt => {
     localStorage.setItem('videoplayer-current-time', evt.seconds);
 };
