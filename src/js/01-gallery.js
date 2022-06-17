45297dd8d355ab3f37dfff7c9ea91e65b7c05822
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-17 12:45:32 +0300

    update 01-gallery no styles

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+const imagesContainer = document.querySelector('.gallery');
+const imagesCardsMarkup = createImagesCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesCardsMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createImagesCardsMarkup(images) {
+  return images
+    .map(image => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                    <img 
+                    class="gallery__image" 
+                    src="${image.preview}" 
+                    alt="${image.description}"
+                    />
+                </a>
+            </div>
+        `;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
