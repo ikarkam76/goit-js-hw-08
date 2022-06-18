1073cfcab9287b07506558ffd606118610dc83b2
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-18 21:44:47 +0300

    correct 03 line 32-43

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,36 +7,38 @@ const feedbackForm = document.querySelector('.feedback-form');
 const formEmail = document.querySelector('.feedback-form input');
 const formMessage = document.querySelector('.feedback-form textarea');
 
-populateFormInput();
-
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 feedbackForm.addEventListener('submit', onFormSubmit);
 
 function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localFormData = JSON.stringify(formData);
-    localStorage.setItem(STORAGE_DATA_KEY, localFormData);
+  formData[evt.target.name] = evt.target.value;
+  localFormData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_DATA_KEY, localFormData);
+}
+
+populateFormInput();
+
+function populateFormInput() {
+  if (STORAGE_DATA_KEY) {
+    const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
+    formEmail.value = savedFormInput.email;
+    formMessage.value = savedFormInput.message;
+  } else {
+    formEmail.value = '';
+    formMessage.value = '';
+  }
 }
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    const submitFormData = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-    console.log(submitFormData);
+  evt.preventDefault();
+  const submitFormData = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
+  // for (const key of submitFormData) {
+  //     if (submitFormData[key] === null) {
+  //       alert(`Fild ${key} must be filled`);
+  //     }
+  //     console.log(submitFormData[key]);
+  // }
 
-    localStorage.removeItem(STORAGE_DATA_KEY);
-    evt.target.reset();
+  localStorage.removeItem(STORAGE_DATA_KEY);
+  evt.target.reset();
 }
-
-function populateFormInput() {
-   
-    if (STORAGE_DATA_KEY !== 0) {
-      const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-      formEmail.value = savedFormInput.email;
-      formMessage.value = savedFormInput.message;
-    } else {
-      formEmail.value = '';
-      formMessage.value = '';
-    }
-    
-}
-
