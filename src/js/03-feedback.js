798d2b85c8735974258edc503618438bc00c6947
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-20 09:29:33 +0300

    change 03 line 20

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,12 +8,13 @@ const formEmail = document.querySelector('.feedback-form input');
 const formMessage = document.querySelector('.feedback-form textarea');
 
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
-feedbackForm.addEventListener('submit', onFormSubmit);
+// feedbackForm.addEventListener('submit', onFormSubmit);
 
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
   localFormData = JSON.stringify(formData);
   localStorage.setItem(STORAGE_DATA_KEY, localFormData);
+  console.log(formData);
 }
 
 populateFormInput();
@@ -21,24 +22,8 @@ populateFormInput();
 function populateFormInput() {
   if (STORAGE_DATA_KEY) {
     const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
+    console.log(savedFormInput);
     formEmail.value = savedFormInput.email;
     formMessage.value = savedFormInput.message;
-  } else {
-    formEmail.value = '';
-    formMessage.value = '';
-  }
-}
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  const submitFormData = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-  // for (const key of submitFormData) {
-  //     if (submitFormData[key] === null) {
-  //       alert(`Fild ${key} must be filled`);
-  //     }
-  //     console.log(submitFormData[key]);
-  // }
-
-  localStorage.removeItem(STORAGE_DATA_KEY);
-  evt.target.reset();
+  } 
 }
