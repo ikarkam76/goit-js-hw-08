eaf7190b73234f4e282561e98f05f3475bd50727
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-20 10:55:36 +0300

    correct 03 function populateforminput

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,22 +8,33 @@ const formEmail = document.querySelector('.feedback-form input');
 const formMessage = document.querySelector('.feedback-form textarea');
 
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
-// feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('submit', onFormSubmit);
 
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
   localFormData = JSON.stringify(formData);
   localStorage.setItem(STORAGE_DATA_KEY, localFormData);
-  console.log(formData);
 }
 
 populateFormInput();
 
 function populateFormInput() {
-  if (STORAGE_DATA_KEY) {
-    const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-    console.log(savedFormInput);
-    formEmail.value = savedFormInput.email;
-    formMessage.value = savedFormInput.message;
+  const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
+  if (savedFormInput) {
+    if (savedFormInput.email) {
+        formEmail.value = savedFormInput.email;
+    }
+    if (savedFormInput.message) {
+        formMessage.value = savedFormInput.message;
+    }
   } 
+      console.log(savedFormInput);
+} 
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+
 }
