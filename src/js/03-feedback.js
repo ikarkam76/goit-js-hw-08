f762f98bc8c08f633c6d7f65080a1afc2016a98e
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-20 11:40:19 +0300

    correct formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,16 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_DATA_KEY = 'feedback-form-state';
-const formData = {};
-
 const feedbackForm = document.querySelector('.feedback-form');
 const formEmail = document.querySelector('.feedback-form input');
 const formMessage = document.querySelector('.feedback-form textarea');
+const STORAGE_DATA_KEY = 'feedback-form-state';
+
+populateFormInput();
+
+const formData = {
+  email: formEmail.value,
+  message: formMessage.value, 
+};
 
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 feedbackForm.addEventListener('submit', onFormSubmit);
@@ -13,20 +18,19 @@ feedbackForm.addEventListener('submit', onFormSubmit);
 function onFormInput(evt) {
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_DATA_KEY, JSON.stringify(formData));
-   console.log(JSON.stringify(formData));
+   console.log(formData);
 }
 
-populateFormInput();
 
 function populateFormInput() {
   const savedFormInput = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
   if (savedFormInput) {
     if (savedFormInput.email) {
         formEmail.value = savedFormInput.email;
-    }
+    };
     if (savedFormInput.message) {
         formMessage.value = savedFormInput.message;
-    }
+    };
   } 
 } 
 
@@ -34,6 +38,6 @@ function populateFormInput() {
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  evt.currentTarget.reset();
+  // evt.currentTarget.reset();
 
 }
